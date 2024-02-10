43c55a62dfda5b39725cf04dc6d16358f3628b3e
donny17-bit <doniwahyu14@gmail.com>
2024-02-10 11:48:20 +0700

    initial commit

---
+++ filemoon.py
@@ -0,0 +1,139 @@
+from selenium import webdriver
+import time
+import random
+
+
+# variables
+wait_start = 1
+wait_end = 5
+duration = 2
+iphub = "https://iphub.info/"
+isCheckIp = True
+# drivers = []
+headless = False
+with_proxy = False
+
+
+def browser_actions(driver, url):
+    xpath_iphub = '//*[@id="hostname"]'
+    xpath_country = '//*[@id="countryName"]'
+    xpath_type = '//*[@id="type"]'
+    xpath_play = '//*[@id="vplayer"]/div[2]/div[12]/div[1]/div/div/div[2]/div'
+
+    if wait_start == wait_end:
+        wait_time = wait_start
+    else:
+        wait_time = random.randrange(wait_start, wait_end)
+
+        try:
+            # check your ip
+            if isCheckIp:
+                driver.get(iphub)
+                time.sleep(10)
+
+                element_ip = driver.find_element("xpath", xpath_iphub)
+                element_country = driver.find_element("xpath", xpath_country)
+                element_type = driver.find_element("xpath", xpath_type)
+
+                ip = element_ip.text
+                country = element_country.text
+                type = element_type.text
+
+                print("Your IP is :", ip)
+                print("Your country is :", country)
+                print("Your IP type is :", type)
+
+            print(f"menuju alamat video : {url}")
+            print("")
+            driver.get(url)
+
+            button = driver.find_element("xpath", xpath_play)
+            button.click()
+
+            # close iklan
+            # Get the handles of all open windows
+            all_window_handles = driver.window_handles
+
+            # Switch to the second window (index 1 in 0-based indexing)
+            driver.switch_to.window(all_window_handles[1])
+
+            # Close the second window
+            driver.close()
+
+            # Switch back to the main window
+            driver.switch_to.window(all_window_handles[0])
+
+            print("Memutar video di browser ", driver)
+            time.sleep(duration)
+
+            print("selesai memutar video, menunggu driver menutup")
+            time.sleep(wait_time)
+            driver.quit()
+
+        except Exception as e:
+            raise
+
+
+def browser(index_proxy):
+    if with_proxy:
+        # Read proxy from the text file
+        with open("proxy.txt", "r") as file:
+            proxys = file.read().splitlines()
+
+        # pilih proxy yang ingin dibuka
+        # proxy_picker = random.randrange(0, len(proxys) - 1, 1)
+
+        # print("Using proxy : ", proxys[proxy_picker])
+        print("Using proxy : ", proxys[index_proxy])
+
+        # Set up the SOCKS5 proxy
+        # http_proxy = f"socks5://{proxys[proxy_picker]}"
+        http_proxy = f"socks5://{proxys[index_proxy]}"
+
+    chrome_options = webdriver.ChromeOptions()
+    if headless:
+        chrome_options.add_argument("--headless")
+    else:
+        chrome_options.add_argument("--window-size=0,0")
+    if with_proxy:
+        chrome_options.add_argument(f"--proxy-server={http_proxy}")
+    chrome_options.add_argument("--disable-logging")
+    chrome_options.add_argument("--log-level=3")
+
+    # Create WebDriver instances for each browser
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+
+def main(index_proxy):
+    while True:
+        try:
+            driver = browser(index_proxy)
+
+            # Read URLs from the text file
+            with open("filemoon.txt", "r") as file:
+                urls = file.read().splitlines()
+
+            # pilih link yang ingin dibuka
+            url_picker = random.randrange(0, len(urls) - 1, 1)
+
+            # do action with the browser
+            browser_actions(driver, urls[url_picker])
+            break
+
+        except Exception as e:
+            # old error logic
+            # if "unknown error: net::ERR_PROXY_CONNECTION_FAILED" in str(e):
+            #     print("proxy error occured ")
+            #     # print("change proxy server ")
+            #     # driver = browser()
+            #     # browser_actions(driver, url)
+
+            #     # error = True
+            #     # index_proxys = index_proxys + 1
+            #     time.sleep(5)
+            #     continue
+            # else:
+            #     raise
+            raise
+        # break
---
+++ main.py
@@ -0,0 +1,171 @@
+import filemoon
+from threading import Thread, Event
+import time
+
+filemoon.headless = False
+filemoon.isCheckIp = True
+filemoon.wait_start = 5
+filemoon.wait_end = 20
+filemoon.duration = 5
+filemoon.with_proxy = False
+loops = 3
+browser = 2
+
+
+def headless_input():
+    headless = input("Hide the browser(y/n) : ")
+    headlesss = headless.lower()
+
+    if headlesss == "y":
+        filemoon.headless = True
+    elif headlesss == "n":
+        filemoon.headless = False
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")
+        headless_input()
+
+
+def isCheckIp_input():
+    isCheckIp = input("Wanna check the ip(y/n) : ")
+    isCheckIpp = isCheckIp.lower()
+
+    if isCheckIpp == "y":
+        filemoon.isCheckIp = True
+    elif isCheckIpp == "n":
+        filemoon.isCheckIp = False
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")
+        isCheckIp_input()
+
+
+def isProxy_input():
+    proxy = input("Wanna use proxy(y/n) : ")
+    proxyy = proxy.lower()
+
+    if proxyy == "y":
+        filemoon.with_proxy = True
+    elif proxyy == "n":
+        filemoon.with_proxy = False
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")
+        isProxy_input()
+
+
+def is_number(input_str, input_str1):
+    try:
+        float(input_str)  # Try converting the input to a float
+        float(input_str1)  # Try converting the input to a float
+        return True  # Return True if successful
+    except ValueError:
+        return False  # Return False if ValueError occurs
+
+
+def wait_input():
+    wait_start = input("Enter start time to wait after watch video (seconds) : ")
+    wait_end = input("Enter end time to wait after watch video (seconds) : ")
+
+    if is_number(wait_start, wait_end):
+        if int(wait_start) > int(wait_end):
+            print("wait start must be lower than wait end")
+            wait_input()
+        else:
+            filemoon.wait_start = int(wait_start)
+            filemoon.wait_end = int(wait_end)
+    else:
+        print("Invalid input. Please number in second")
+        wait_input()
+
+
+def duration_input():
+    durations = input("Enter the video duration (seconds) : ")
+
+    if is_number(durations, 0):
+        filemoon.duration = int(durations)
+    else:
+        print("Invalid input. Please number in second")
+        duration_input()
+
+
+def loop_input():
+    loop = input("How many times you wanna watch (number) : ")
+    global loops
+    if is_number(loop, 0):
+        loops = int(loop)
+    else:
+        print("Invalid input. Please input number")
+        loop_input()
+
+
+def browser_input():
+    browsers = input("How many thread you wanna use (number) : ")
+    global browser
+
+    if is_number(browsers, 0):
+        browser = int(browsers)
+    else:
+        print("Invalid input. Please input number")
+        browser_input()
+
+
+threads = []
+success = 0
+failed = 0
+revenue = 0.003
+stop = True
+proxy = []
+
+
+def run():
+    try:
+        headless_input()
+        isCheckIp_input()
+        isProxy_input()
+        # wait_input()
+        duration_input()
+        loop_input()
+        browser_input()
+
+        with open("proxy.txt", "r") as file:
+            proxy = file.read().splitlines()
+
+        proxy_picker = len(proxy) - 1
+        current_proxy = 0
+
+        # wrapper to catch thread error
+        def filemoon_func(current_proxy):
+            try:
+                filemoon.main(current_proxy)
+            except Exception as e:
+                if "unknown error: net::ERR_PROXY_CONNECTION_FAILED" in str(e):
+                    print("proxy error occured ")
+                else:
+                    print("An error occurred in thread")
+
+        # run the thread
+        for n in range(loops):
+            print("current proxy : ", proxy[current_proxy])
+
+            for i in range(browser):
+                # thread = Thread(target=browser_main_wrapper, args=(i,))
+                thread = Thread(target=filemoon_func, args=(current_proxy,))
+                thread.start()
+                threads.append(thread)
+
+            # Wait for all threads to complete before moving to the next iteration
+            for thread in threads:
+                thread.join()
+
+            # pick proxy sequential from proxy list
+            if current_proxy == proxy_picker:
+                current_proxy = 0
+            else:
+                current_proxy = current_proxy + 1
+
+    # except Exception as e:
+    #     print("error occured in main file : ", e)
+    finally:
+        print("Filemoon Bot views done")
+
+
+if __name__ == "__main__":
+    run()
